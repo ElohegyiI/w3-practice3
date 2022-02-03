54e689a02b5a69b73c334bd0a999a3d92b5e9cf6
ElohegyiI <roxnadz78@gmail.com>
2022-02-03 16:02:22 +0100

    Próba

---
+++ frontend/script.js
@@ -0,0 +1,24 @@
+//function addTwoIntegers(firstInteger, secondInteger) {
+    //console.log(firstInteger + secondInteger)
+    //return firstInteger + secondInteger
+//if(typeof firstInteger === "number" && typeof secondInteger === "number") {return firstInteger + secondInteger
+//} else {
+  //  return "valamelyik argumentum nem szám"
+//}  
+//}
+//console.log( addTwoIntegers(12,13) );
+//console.log( addTwoIntegers(1920,85) );
+//console.log(0=="0")
+
+function addThreeIntegers (firstInteger, secondInteger,thirdInteger) {
+    
+    return (firstInteger - secondInteger + thirdInteger)
+
+}
+console.log( addThreeIntegers(50, 20, "y"));
+if (typeof firstInteger === "number" && typeof secondInteger === "number" &&typeof thirdInteger === "number") {
+    return (firstInteger - secondInteger + thirdInteger);
+} else { return "valamelyik nem szám"
+}
+console.log(addThreeIntegers(50, 20, 0));
+console.log (0=="0")
